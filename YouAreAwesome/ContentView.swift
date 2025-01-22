af31d32469df546fc7d98f8622575a5707dc9ac8
Benjamin Schmidt <Benny@schmidtb.de>
2025-01-22 16:47:03 +0100

    constants, variables and scope

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
@@ -26,14 +26,19 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    message = "Awesome!"
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                if message == message1 {
+                    message = message2
+                    imageName = imageString2
+                } else {
+                    message = message1
+                    imageName = imageString1
                 }
                 
-                Button("Great") {
-                    message = "Great!"
-                }
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
