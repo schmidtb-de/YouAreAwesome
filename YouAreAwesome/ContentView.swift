a1095b77f9981e35b3e7f5c843ba97a41f10d66d
Benjamin Schmidt <Benny@schmidtb.de>
2025-01-23 16:03:20 +0100

    ternary operator

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -31,14 +31,17 @@ struct ContentView: View {
                 let message2 = "You are Great!"
                 let imageString1 = "hand.thumbsup"
                 let imageString2 = "sun.max.fill"
-                if message == message1 {
-                    message = message2
-                    imageName = imageString2
-                } else {
-                    message = message1
-                    imageName = imageString1
-                }
+//                if message == message1 {
+//                    message = message2
+//                    imageName = imageString2
+//                } else {
+//                    message = message1
+//                    imageName = imageString1
+//                }
+//
                 
+                message = (message == message1 ? message2 : message1)
+                imageName = (imageName == imageString1 ? imageString2 : imageString1)
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
