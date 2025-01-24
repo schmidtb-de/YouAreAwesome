058ef707dcc454084fde774fed1ea152f6a90026
Benjamin Schmidt <Benny@schmidtb.de>
2025-01-24 16:28:13 +0100

    arrays!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     
     var body: some View {
         
@@ -27,14 +28,27 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
             
             Spacer()
             
             Button("Sow Message!") {
-                let message1 = "You are Awesome!"
-                let message2 = "You are Great!"
                 
-                message = (message == message1 ? message2 : message1)
+                let messages = ["You are Awesome!",
+                                "You are Great!",
+                                "You are the Man!",
+                                "You are Amazing!",
+                                "You are incredible!",
+                                "Never give up!"]
+                
+                message = messages[messageNumber]
+                
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
                 
                 //TODO: - Update the imageName variable -
                 imageName = "image\(imageNumber)"
