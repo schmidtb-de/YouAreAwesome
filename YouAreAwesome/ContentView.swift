da9bc401c133b9b62d0b41af9c5b588b522ccc56
Benjamin Schmidt <Benny@schmidtb.de>
2025-01-26 03:27:53 +0100

    scale factor and animations

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,19 +16,22 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
             
             Spacer()
             
